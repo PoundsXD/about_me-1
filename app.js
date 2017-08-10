d2782053ce389d3c1ce4c351b61eab4047be30a6
Tiger Hsu <kymiehsu123@gmail.com>
2017-08-09 23:59:10 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 'use strict';
+var correct = 0;
+
 //question1 dog owner
+
 var animalLike = prompt ('Do you think I have a dog?');
 var animalLike1 = animalLike.toUpperCase();
 
 console.log(animalLike1);
 if(animalLike1 === 'YES' || 'Y' === animalLike1){
   alert('yes I use to have golden');
+  correct++;
 } else if (animalLike1 === 'NO' || 'N' === animalLike1) {
   alert ('NO why how could you?');
 } else {
@@ -18,6 +22,7 @@ var cook1 = cook.toUpperCase();
 console.log(cook1);
 if(cook1 === 'YES' || 'Y' === cook1){
   alert('yes I use to be professional chef!');
+  correct++
 } else if (cook1 === 'NO' || 'N' === cook1) {
   alert ('no? how do you think I cook for myself?');
 } else {
@@ -31,6 +36,7 @@ var military1 = military.toUpperCase();
 console.log(military1);
 if( military1 === 'YES' || 'Y' === military1){
   alert('yes, I use to serve in the united states Coast Guard');
+  correct++;
 } else if (military1 === 'NO' || 'N' === military1) {
   alert ('No? so you think i was a bum huh?');
 } else {
@@ -43,6 +49,7 @@ var nation1 = nation.toUpperCase();
 console.log(military1);
 if( nation1 === 'YES' || 'Y' === nation1 ){
   alert('yes,we are all immigrants from somewhere');
+  correct++;
 } else if (nation1 === 'NO' || 'N' === nation1) {
   alert ('No? so where the hell did you think I was from?');
 } else {
@@ -56,8 +63,40 @@ var writtingHand1 = writtingHand.toUpperCase();
 console.log(writtingHand1);
 if( writtingHand1 === 'YES' || 'Y' === writtingHand1){
   alert('yes 60% of all people write with their right hand');
+  correct++;
 } else if (writtingHand1 === 'NO' || 'N' === writtingHand1) {
   alert ('No, well Im certainly not left handed!');
 } else {
   alert('PLEASE STICK TO YES OR NO ANSWERS!!!');
 };
+
+//question 6
+var i = 0;
+for(i = 0; i < 4; i++){
+  var guessAge = prompt('Can you guess my age?');
+  var guessAgeAnswer = guessAge;
+  console.log(guessAgeAnswer);
+  if (guessAgeAnswer == 30){
+    alert('YAH YAH YOU GUESSED CORRECTLY!!');
+    correct++;
+  } else if(guessAgeAnswer < 30){
+    alert('you are too low');
+  } else if(guessAgeAnswer > 30){
+    alert('you are too high');
+  } else if(guessAgeAnswer >= 28){
+    alert('you are getting close');
+  }
+}
+
+for (var i = 6; i > 0 ; i--) {
+  var myArray = ['CHOCOLATE','VANILLA', 'BLUEBERRY'];
+  var food = prompt('Can you guess what type of Ice cream I have?').toUpperCase();
+  if (food === myArray[0] || food === myArray[1] || food === myArray[2]){
+    alert ('this is correct!');
+    correct++;
+  } else {
+    alert ('wrong answer!');
+  }
+}
+
+console.log(correct);
